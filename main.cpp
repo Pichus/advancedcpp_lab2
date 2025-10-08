0f35753818da3713db96b83c836a01f4314112c7
pichus <dimatyzuk@gmail.com>
2025-10-08 21:07:25 +0300

    fix errors + improve code quality and readability

--- main.cpp
+++ main.cpp
@@ -6,35 +6,11 @@
 #include <functional>
 #include <iostream>
 #include <latch>
+#include <map>
 #include <random>
 
-bool IsEven(const int number) { return number % 2 == 0; }
-
-void TestAllOfWithoutPolicy(const std::vector<int> &sequence) {
-    auto _ = std::all_of(sequence.begin(), sequence.end(),
-                         [](const int number) { return IsEven(number); });
-}
-
-void TestAllOfWithSequencedPolicy(const std::vector<int> &sequence) {
-    auto _ = std::all_of(std::execution::seq, sequence.begin(), sequence.end(),
-                         [](const int number) { return IsEven(number); });
-}
-
-void TestAllOfWithParallelPolicy(const std::vector<int> &sequence) {
-    auto _ = std::all_of(std::execution::par, sequence.begin(), sequence.end(),
-                         [](const int number) { return IsEven(number); });
-}
-
-void TestAllOfWithParallelUnsequencedPolicy(const std::vector<int> &sequence) {
-    auto _ =
-        std::all_of(std::execution::par_unseq, sequence.begin(), sequence.end(),
-                    [](const int number) { return IsEven(number); });
-}
-
-void TestAllOfWithUnsequencedPolicy(const std::vector<int> &sequence) {
-    auto _ =
-        std::all_of(std::execution::unseq, sequence.begin(), sequence.end(),
-                    [](const int number) { return IsEven(number); });
+bool MyPredicate(const int number) {
+    return (2 * atan(tanh(number / 2.0))) > 0;
 }
 
 template <typename Function, typename... Arguments>
@@ -53,7 +29,7 @@ std::vector<int> GenerateRandomSequence(const std::size_t sequence_size,
     std::vector<int> random_sequence;
     random_sequence.reserve(sequence_size);
 
-    std::uniform_int_distribution<std::mt19937::result_type> distribution(1, 6);
+    std::uniform_int_distribution<std::mt19937::result_type> distribution(2, 8);
 
     for (int i = 0; i < sequence_size; i++) {
         random_sequence.push_back(distribution(mersenne_twister_engine));
@@ -84,11 +60,12 @@ void TestAllOfUsingMultipleThreads(
     const std::vector<std::vector<int>> &sequence_chunks) {
     std::vector<std::jthread> threads;
     std::latch work_done(sequence_chunks.size());
+
     for (int i = 0; i < sequence_chunks.size(); i++) {
         threads.emplace_back([sequence_chunks, i, &work_done]() {
             auto _ = std::ranges::all_of(
                 sequence_chunks[i],
-                [](const int number) { return IsEven(number); });
+                [](const int number) { return MyPredicate(number); });
             work_done.count_down();
         });
     }
@@ -96,87 +73,130 @@ void TestAllOfUsingMultipleThreads(
     work_done.wait();
 }
 
+void part1(const std::vector<std::vector<int>> &sequences) {
+    std::cout << "<start> No policy\n";
+
+    for (int i = 0; i < sequences.size(); i++) {
+        std::cout << std::format("sequence size = {} ; ", sequences[i].size())
+                  << "time = "
+                  << MeasureFunctionExecutionTime([sequences, i]() {
+                         auto _ =
+                             std::ranges::all_of(sequences[i], MyPredicate);
+                     })
+                  << std::endl;
+    }
+    std::cout << "<end>\n\n" << std::endl;
+}
+
+void part2(const std::vector<std::vector<int>> &sequences) {
+    std::cout << "<start> Sequenced policy\n";
+    for (int i = 0; i < sequences.size(); i++) {
+        std::cout << std::format("sequence size = {} ; ", sequences[i].size())
+                  << "time = "
+                  << MeasureFunctionExecutionTime([sequences, i]() {
+                         auto _ = std::all_of(std::execution::seq,
+                                              sequences[i].begin(),
+                                              sequences[i].end(), MyPredicate);
+                     })
+                  << std::endl;
+    }
+    std::cout << "<end>\n\n" << std::endl;
+
+    std::cout << "<start> Parallel policy\n";
+    for (int i = 0; i < sequences.size(); i++) {
+        std::cout << std::format("sequence size = {} ; ", sequences[i].size())
+                  << "time = "
+                  << MeasureFunctionExecutionTime([sequences, i]() {
+                         auto _ = std::all_of(std::execution::par,
+                                              sequences[i].begin(),
+                                              sequences[i].end(), MyPredicate);
+                     })
+                  << std::endl;
+    }
+    std::cout << "<end>\n\n" << std::endl;
+
+    std::cout << "<start> Parallel unsequenced policy\n";
+    for (int i = 0; i < sequences.size(); i++) {
+        std::cout << std::format("sequence size = {} ; ", sequences[i].size())
+                  << "time = "
+                  << MeasureFunctionExecutionTime([sequences, i]() {
+                         auto _ = std::all_of(std::execution::par_unseq,
+                                              sequences[i].begin(),
+                                              sequences[i].end(), MyPredicate);
+                     })
+                  << std::endl;
+    }
+    std::cout << "<end>\n\n" << std::endl;
+
+    std::cout << "<start> Unsequenced policy\n";
+    for (int i = 0; i < sequences.size(); i++) {
+        std::cout << std::format("sequence size = {} ; ", sequences[i].size())
+                  << "time = "
+                  << MeasureFunctionExecutionTime([sequences, i]() {
+                         auto _ = std::all_of(std::execution::unseq,
+                                              sequences[i].begin(),
+                                              sequences[i].end(), MyPredicate);
+                     })
+                  << std::endl;
+    }
+    std::cout << "<end>\n\n" << std::endl;
+}
+
+struct SplitSequence {
+    std::vector<std::vector<int>> chunks;
+    int size;
+};
+
+void part3(const std::vector<std::vector<int>> &sequences) {
+    std::map<int, std::vector<SplitSequence>> split_sequences_map;
+
+    for (int i = 0; i < sequences.size(); i++) {
+        for (int k = 2; k < 12; k += 2) {
+            if (!split_sequences_map.contains(k)) {
+                split_sequences_map[k] = std::vector<SplitSequence>();
+            }
+
+            SplitSequence split_sequence;
+            split_sequence.chunks = SplitVectorIntoKVectors(sequences[i], k);
+            split_sequence.size = sequences[i].size();
+
+            split_sequences_map[k].push_back(split_sequence);
+        }
+    }
+
+    for (auto const &[key, value] : split_sequences_map) {
+        std::cout << std::format("<start> Multithreaded k = {} \n", key);
+
+        for (int i = 0; i < value.size(); i++) {
+            std::cout << std::format("sequence size = {} ; ", value[i].size)
+                      << "time = "
+                      << MeasureFunctionExecutionTime(
+                             TestAllOfUsingMultipleThreads, value[i].chunks)
+                      << std::endl;
+        }
+
+        std::cout << "<end>\n\n" << std::endl;
+    }
+}
+
 int main() {
     std::random_device random_device;
     std::mt19937 mersenne_twister_engine(random_device());
 
-    const auto seq1 = GenerateRandomSequence(10000, mersenne_twister_engine);
-    const auto seq2 = GenerateRandomSequence(100000, mersenne_twister_engine);
-    const auto seq3 = GenerateRandomSequence(1000000, mersenne_twister_engine);
+    constexpr int sequence_count = 5;
+    std::vector<std::vector<int>> sequences;
+    int starting_sequence_size = 100;
+    sequences.reserve(sequence_count);
 
-    // part 1
-    std::cout << "<start> No policy\n";
-    std::cout << MeasureFunctionExecutionTime(TestAllOfWithoutPolicy, seq1)
-              << std::endl;
-    std::cout << MeasureFunctionExecutionTime(TestAllOfWithoutPolicy, seq2)
-              << std::endl;
-    std::cout << MeasureFunctionExecutionTime(TestAllOfWithoutPolicy, seq3)
-              << std::endl;
-    std::cout << "<end>\n\n" << std::endl;
+    for (int i = 0; i < sequence_count; i++) {
+        sequences.push_back(GenerateRandomSequence(starting_sequence_size,
+                                                   mersenne_twister_engine));
+        starting_sequence_size *= 10;
+    }
 
-    // part 2
-    std::cout << "<start> Sequenced policy\n";
-    std::cout << MeasureFunctionExecutionTime(TestAllOfWithSequencedPolicy,
-                                              seq1)
-              << std::endl;
-    std::cout << MeasureFunctionExecutionTime(TestAllOfWithSequencedPolicy,
-                                              seq2)
-              << std::endl;
-    std::cout << MeasureFunctionExecutionTime(TestAllOfWithSequencedPolicy,
-                                              seq3)
-              << std::endl;
-    std::cout << "<end>\n\n" << std::endl;
-
-    std::cout << "<start> Parallel policy\n";
-    std::cout << MeasureFunctionExecutionTime(TestAllOfWithParallelPolicy, seq1)
-              << std::endl;
-    std::cout << MeasureFunctionExecutionTime(TestAllOfWithParallelPolicy, seq2)
-              << std::endl;
-    std::cout << MeasureFunctionExecutionTime(TestAllOfWithParallelPolicy, seq3)
-              << std::endl;
-    std::cout << "<end>\n\n" << std::endl;
-
-    std::cout << "<start> Parallel unsequenced policy\n";
-    std::cout << MeasureFunctionExecutionTime(
-                     TestAllOfWithParallelUnsequencedPolicy, seq1)
-              << std::endl;
-    std::cout << MeasureFunctionExecutionTime(
-                     TestAllOfWithParallelUnsequencedPolicy, seq2)
-              << std::endl;
-    std::cout << MeasureFunctionExecutionTime(
-                     TestAllOfWithParallelUnsequencedPolicy, seq3)
-              << std::endl;
-    std::cout << "<end>\n\n" << std::endl;
-
-    std::cout << "<start> Unsequenced policy\n";
-    std::cout << MeasureFunctionExecutionTime(TestAllOfWithUnsequencedPolicy,
-                                              seq1)
-              << std::endl;
-    std::cout << MeasureFunctionExecutionTime(TestAllOfWithUnsequencedPolicy,
-                                              seq2)
-              << std::endl;
-    std::cout << MeasureFunctionExecutionTime(TestAllOfWithUnsequencedPolicy,
-                                              seq3)
-              << std::endl;
-    std::cout << "<end>\n\n" << std::endl;
-
-    // part 3
-    const int k = 100;
-    const auto seq1_split = SplitVectorIntoKVectors(seq1, k);
-    const auto seq2_split = SplitVectorIntoKVectors(seq2, k);
-    const auto seq3_split = SplitVectorIntoKVectors(seq3, k);
-
-    std::cout << "<start> Multithreaded \n";
-    std::cout << MeasureFunctionExecutionTime(TestAllOfUsingMultipleThreads,
-                                              seq1_split)
-              << std::endl;
-    std::cout << MeasureFunctionExecutionTime(TestAllOfUsingMultipleThreads,
-                                              seq2_split)
-              << std::endl;
-    std::cout << MeasureFunctionExecutionTime(TestAllOfUsingMultipleThreads,
-                                              seq3_split)
-              << std::endl;
-    std::cout << "<end>\n\n" << std::endl;
+    part1(sequences);
+    part2(sequences);
+    part3(sequences);
 
     return 0;
 }
