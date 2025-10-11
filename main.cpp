ab6599604666e131a557d38eac659496e8943107
pichus <dimatyzuk@gmail.com>
2025-10-11 14:06:43 +0300

    improve naming

--- main.cpp
+++ main.cpp
@@ -3,11 +3,13 @@
 
 #include <chrono>
 #include <execution>
+#include <format>
 #include <functional>
 #include <iostream>
 #include <latch>
 #include <map>
 #include <random>
+#include <vector>
 
 bool MyPredicate(const int number) {
     return (2 * atan(tanh(number / 2.0))) > 0;
@@ -15,12 +17,13 @@ bool MyPredicate(const int number) {
 
 template <typename Function, typename... Arguments>
 std::chrono::duration<double, std::milli> MeasureFunctionExecutionTime(
-    const Function &&function, const Arguments &...arguments) {
+    Function &&function, const Arguments &...arguments) {
     const auto time_point_before_function_call =
         std::chrono::high_resolution_clock::now();
     function(arguments...);
     const auto time_point_after_function_call =
         std::chrono::high_resolution_clock::now();
+
     return time_point_after_function_call - time_point_before_function_call;
 }
 
@@ -39,17 +42,17 @@ std::vector<int> GenerateRandomSequence(const std::size_t sequence_size,
 }
 
 template <typename VectorElementType>
-std::vector<std::vector<int>> SplitVectorIntoKVectors(
-    const std::vector<VectorElementType> &vector_to_split, int k) {
-    std::vector<std::vector<int>> chunks(k);
-    const int chunk_size = vector_to_split.size() / k;
-    const int remainder = vector_to_split.size() % k;
+std::vector<std::vector<int>> SplitSequenceIntoChunks(
+    const std::vector<VectorElementType> &sequence, int chunk_count) {
+    std::vector<std::vector<int>> chunks(chunk_count);
+    const int chunk_size = sequence.size() / chunk_count;
+    const int remainder = sequence.size() % chunk_count;
 
     int start = 0;
-    for (int i = 0; i < k; ++i) {
+    for (int i = 0; i < chunk_count; ++i) {
         const int end = start + chunk_size + (i < remainder ? 1 : 0);
-        chunks[i] = std::vector(vector_to_split.begin() + start,
-                                vector_to_split.begin() + end);
+        chunks[i] =
+            std::vector(sequence.begin() + start, sequence.begin() + end);
         start = end;
     }
 
@@ -62,7 +65,7 @@ void TestAllOfUsingMultipleThreads(
     std::latch work_done(sequence_chunks.size());
 
     for (const auto &sequence_chunk : sequence_chunks) {
-        threads.emplace_back([sequence_chunks, &sequence_chunk, &work_done]() {
+        threads.emplace_back([&sequence_chunk, &work_done]() {
             auto _ = std::ranges::all_of(sequence_chunk, [](const int number) {
                 return MyPredicate(number);
             });
@@ -73,105 +76,105 @@ void TestAllOfUsingMultipleThreads(
     work_done.wait();
 }
 
-void part1(const std::vector<std::vector<int>> &sequences) {
+void Part1(const std::vector<std::vector<int>> &sequences) {
     std::cout << "<start> No policy\n";
 
-    for (const auto & sequence : sequences) {
+    for (const auto &sequence : sequences) {
         std::cout << std::format("sequence size = {} ; ", sequence.size())
-                  << "time = "
-                  << MeasureFunctionExecutionTime([sequences, &sequence]() {
-                         auto _ =
-                             std::ranges::all_of(sequence, MyPredicate);
+                  << "time = " << MeasureFunctionExecutionTime([&sequence]() {
+                         auto _ = std::ranges::all_of(sequence, MyPredicate);
                      })
                   << std::endl;
     }
     std::cout << "<end>\n\n" << std::endl;
 }
 
-void part2(const std::vector<std::vector<int>> &sequences) {
+void Part2(const std::vector<std::vector<int>> &sequences) {
     std::cout << "<start> Sequenced policy\n";
-    for (const auto & sequence : sequences) {
+    for (const auto &sequence : sequences) {
         std::cout << std::format("sequence size = {} ; ", sequence.size())
-                  << "time = "
-                  << MeasureFunctionExecutionTime([sequences, &sequence]() {
-                         auto _ = std::all_of(std::execution::seq,
-                                              sequence.begin(),
-                                              sequence.end(), MyPredicate);
+                  << "time = " << MeasureFunctionExecutionTime([&sequence]() {
+                         auto _ =
+                             std::all_of(std::execution::seq, sequence.begin(),
+                                         sequence.end(), MyPredicate);
                      })
                   << std::endl;
     }
     std::cout << "<end>\n\n" << std::endl;
 
     std::cout << "<start> Parallel policy\n";
-    for (const auto & sequence : sequences) {
+    for (const auto &sequence : sequences) {
         std::cout << std::format("sequence size = {} ; ", sequence.size())
-                  << "time = "
-                  << MeasureFunctionExecutionTime([sequences, &sequence]() {
-                         auto _ = std::all_of(std::execution::par,
-                                              sequence.begin(),
-                                              sequence.end(), MyPredicate);
+                  << "time = " << MeasureFunctionExecutionTime([&sequence]() {
+                         auto _ =
+                             std::all_of(std::execution::par, sequence.begin(),
+                                         sequence.end(), MyPredicate);
                      })
                   << std::endl;
     }
     std::cout << "<end>\n\n" << std::endl;
 
     std::cout << "<start> Parallel unsequenced policy\n";
-    for (const auto & sequence : sequences) {
+    for (const auto &sequence : sequences) {
         std::cout << std::format("sequence size = {} ; ", sequence.size())
-                  << "time = "
-                  << MeasureFunctionExecutionTime([sequences, &sequence]() {
+                  << "time = " << MeasureFunctionExecutionTime([&sequence]() {
                          auto _ = std::all_of(std::execution::par_unseq,
-                                              sequence.begin(),
-                                              sequence.end(), MyPredicate);
+                                              sequence.begin(), sequence.end(),
+                                              MyPredicate);
                      })
                   << std::endl;
     }
     std::cout << "<end>\n\n" << std::endl;
 
     std::cout << "<start> Unsequenced policy\n";
-    for (const auto & sequence : sequences) {
+    for (const auto &sequence : sequences) {
         std::cout << std::format("sequence size = {} ; ", sequence.size())
-                  << "time = "
-                  << MeasureFunctionExecutionTime([sequences, &sequence]() {
+                  << "time = " << MeasureFunctionExecutionTime([&sequence]() {
                          auto _ = std::all_of(std::execution::unseq,
-                                              sequence.begin(),
-                                              sequence.end(), MyPredicate);
+                                              sequence.begin(), sequence.end(),
+                                              MyPredicate);
                      })
                   << std::endl;
     }
     std::cout << "<end>\n\n" << std::endl;
 }
 
-struct SplitSequence {
+struct ChunkedSequence {
     std::vector<std::vector<int>> chunks;
-    int size;
+    std::size_t sequence_size;
 };
 
-void part3(const std::vector<std::vector<int>> &sequences) {
-    std::map<int, std::vector<SplitSequence>> split_sequences_map;
+void Part3(const std::vector<std::vector<int>> &sequences) {
+    std::map<int, std::vector<ChunkedSequence>>
+        chunk_count_to_chunked_sequences_map;
 
-    for (const auto & sequence : sequences) {
-        for (int k = 2; k < 12; k += 2) {
-            if (!split_sequences_map.contains(k)) {
-                split_sequences_map[k] = std::vector<SplitSequence>();
+    for (const auto &sequence : sequences) {
+        for (int k = 2; k < 50; k += 2) {
+            if (!chunk_count_to_chunked_sequences_map.contains(k)) {
+                chunk_count_to_chunked_sequences_map[k] =
+                    std::vector<ChunkedSequence>();
             }
 
-            SplitSequence split_sequence;
-            split_sequence.chunks = SplitVectorIntoKVectors(sequence, k);
-            split_sequence.size = sequence.size();
+            ChunkedSequence split_sequence;
+            split_sequence.chunks = SplitSequenceIntoChunks(sequence, k);
+            split_sequence.sequence_size = sequence.size();
 
-            split_sequences_map[k].push_back(split_sequence);
+            chunk_count_to_chunked_sequences_map[k].push_back(split_sequence);
         }
     }
 
-    for (auto const &[key, value] : split_sequences_map) {
-        std::cout << std::format("<start> Multithreaded k = {} \n", key);
+    for (auto const &[chunk_count, chunked_sequences] :
+         chunk_count_to_chunked_sequences_map) {
+        std::cout << std::format("<start> Multithreaded k = {} \n",
+                                 chunk_count);
 
-        for (const auto & i : value) {
-            std::cout << std::format("sequence size = {} ; ", i.size)
+        for (const auto &chunked_sequence : chunked_sequences) {
+            std::cout << std::format("sequence size = {} ; ",
+                                     chunked_sequence.sequence_size)
                       << "time = "
                       << MeasureFunctionExecutionTime(
-                             TestAllOfUsingMultipleThreads, i.chunks)
+                             TestAllOfUsingMultipleThreads,
+                             chunked_sequence.chunks)
                       << std::endl;
         }
 
@@ -194,9 +197,9 @@ int main() {
         starting_sequence_size *= 10;
     }
 
-    part1(sequences);
-    part2(sequences);
-    part3(sequences);
+    Part1(sequences);
+    Part2(sequences);
+    Part3(sequences);
 
     return 0;
 }
